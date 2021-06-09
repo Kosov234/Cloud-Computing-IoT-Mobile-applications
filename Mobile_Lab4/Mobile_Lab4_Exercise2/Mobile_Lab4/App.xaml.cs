3169f89e470875db50b6b91cf892c83b365b0df3
Kosov234 <kosovkosov@gmail.com>
2021-06-09 16:26:09 +0200

    Adding Mobile_Lab4_Exercise2. Renaming folders.

---
+++ Mobile_Lab4/Mobile_Lab4_Exercise2/Mobile_Lab4/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Mobile_Lab4
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new Mobile_Lab4.MainPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Mobile_Lab4/Mobile_Lab4_Exercise2/Mobile_Lab4/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Mobile_Lab4.Pages;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Mobile_Lab4
+{
+    public partial class MainPage : TabbedPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            this.Children.Add(new FirstPage());
+            this.Children.Add(new SecondPage());
+            this.Children.Add(new ThirdPage());
+            this.Children.Add(new FourthPage());
+        }
+    }
+}
