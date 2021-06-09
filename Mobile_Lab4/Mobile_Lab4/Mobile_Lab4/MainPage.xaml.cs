94791ba10f0cd8c3cdd80d0789c3b1734149a1ac
Kosov234 <kosovkosov@gmail.com>
2021-06-09 15:47:50 +0200

    Adding the first Exercise from Mobile_Lab4

---
+++ Mobile_Lab4/Mobile_Lab4/Mobile_Lab4/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Mobile_Lab4.Pages;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Mobile_Lab4
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            btnFirst.Clicked += (s, e) => Navigation.PushAsync(new FirstPage());
+            btnSecond.Clicked += (s, e) => Navigation.PushAsync(new SecondPage());
+            btnThird.Clicked += (s, e) => Navigation.PushAsync(new ThirdPage());
+            btnFourth.Clicked += (s, e) => Navigation.PushAsync(new FourthPage());
+        }
+    }
+}
---
+++ Mobile_Lab4/Mobile_Lab4/Mobile_Lab4/Pages/SecondPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Mobile_Lab4.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SecondPage : ContentPage
+    {
+        public SecondPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
