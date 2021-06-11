80d1f553618f3a3c4373bbb39abf597bf47641a5
Kosov234 <kosovkosov@gmail.com>
2021-06-11 18:15:50 +0200

    Added Mobile_Lab5.

---
+++ Mobile_Lab5/Mobile_Lab5/Mobile_Lab5/MainPage.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace Mobile_Lab5
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+
+        async void btnLocation_Clicked(object sender, System.EventArgs e)
+        {
+            try
+            {
+                var request = new GeolocationRequest(GeolocationAccuracy.Medium, TimeSpan.FromSeconds(10));
+                var cts = new CancellationTokenSource();
+                var location = await Geolocation.GetLocationAsync(request, cts.Token);
+
+                if (location != null)
+                {
+                    lblLatitude.Text = "Latitude: " + location.Latitude.ToString();
+                    lblLongitude.Text = "Longitude:" + location.Longitude.ToString();
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                await DisplayAlert("Faild", fnsEx.Message, "OK");
+            }
+            catch (PermissionException pEx)
+            {
+                await DisplayAlert("Faild", pEx.Message, "OK");
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Faild", ex.Message, "OK");
+            }
+
+        }
+
+        private async void btnNavigate_Clicked(object sender, EventArgs e)
+        {
+            await NavigateToBuilding25();
+        }
+
+        public async Task NavigateToBuilding25()
+        {
+            var location = new Location(47.645160, -122.1306032);
+            var options = new MapLaunchOptions { NavigationMode = NavigationMode.Driving };
+
+            await Map.OpenAsync(location, options);
+        }
+
+        private async void btnShare_Clicked(object sender, EventArgs e)
+        {
+            var request = new GeolocationRequest(GeolocationAccuracy.Medium, TimeSpan.FromSeconds(10));
+            var cts = new CancellationTokenSource();
+            var location = await Geolocation.GetLocationAsync(request, cts.Token);
+
+            await Share.RequestAsync(new ShareTextRequest
+            {
+                Text = $"Latitude: {location.Latitude.ToString()}. Longtitude: {location.Longitude.ToString()} ",
+                Title = "My current location"
+            });
+        }
+    }
+}
